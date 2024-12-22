b10a073a975762a2b509f19b16b3a3432b30ac46
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2024-12-22 16:19:03 +0100

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,11 @@
 /* Number of PIN where the HIGH should be detected that comes from SIG_PIN */
 const int SWITCH_DETECT_PIN = GPIO_NUM_4;
 
-const int PIN_SERVO_ARM = GPIO_NUM_26;
-const int PIN_SERVO_DOOR = GPIO_NUM_27;
+const int SERVO_GPIO_PIN = GPIO_NUM_27;
 
 Servo servo;
 
 int pos = 0;
-int servoPin = 27;
 
 // Starting position of servo
 int startPos = 270;
@@ -28,22 +26,6 @@ float translateDegrees(int val) {
   if(val < 0) return 0;
   if(val > 270) return 180;
   return (val / 270.0) * 180.0;
-}
-
-void setup() {
-  Serial.begin(9600);
-
-  ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
-
-  servo.setPeriodHertz(333);
-  servo.attach(servoPin, 500, 2500);
-  servo.write(translateDegrees(startPos));
-
-  // Set detect pin as input
-  pinMode(SWITCH_DETECT_PIN, INPUT_PULLUP);
 }
 
 void run() {
@@ -64,7 +46,7 @@ void run() {
   // Go to end position
   for (pos = pausePos; pos > endPos; --pos) { 
 		servo.write(translateDegrees(pos));    
-		delay(4);
+		delay(8);
 	}
 
   Serial.printf("Ended at position: %d \n", pos);
@@ -74,18 +56,51 @@ void run() {
   // Go to start position
 	for (pos = endPos; pos < startPos; ++pos) {
 		servo.write(translateDegrees(pos));
-		delay(4);
+		delay(8);
 	}
 
   Serial.printf("Returned to position: %d \n", pos);
   Serial.println(translateDegrees(pos));
 }
 
+void setup() {
+  Serial.begin(9600);
+
+  servo.attach(SERVO_GPIO_PIN, 500, 2500);
+  servo.write(translateDegrees(startPos));
+
+  // Set detect pin as input
+  pinMode(SWITCH_DETECT_PIN, INPUT_PULLUP);
+}
+
 void loop() {
+  Serial.println(digitalRead(SWITCH_DETECT_PIN));
+
   if(digitalRead(SWITCH_DETECT_PIN) == LOW) {
     run();
   };
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
